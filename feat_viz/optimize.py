38170fdcaaa27f8b0a1d3319ed80edb7bbf7e7ad
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2024-08-30 22:19:46 +0530

    return image at threshold, remove num_iterations
    - Removed `num_iterations` parameter, instead images are collected at the iterations provided by `thresholds` parameter
    - Show the last image if required
    - Option to remove logs and progress bar

--- feat_viz/optimize.py
+++ feat_viz/optimize.py
@@ -6,6 +6,7 @@ from feat_viz.utils import (
     create_simple_logger,
     ImagePlotter,
     is_jupyter_notebook,
+    show_tensor_image,
 )
 from feat_viz.objective import create_objective, Objective, T, M
 
@@ -13,7 +14,7 @@ import torch
 import torch.optim as optim
 
 from tqdm.auto import tqdm
-from typing import Optional, List, Union, Tuple
+from typing import Iterable, Optional, List, Union, Tuple
 import logging
 
 logger = create_simple_logger(__name__)
@@ -59,12 +60,15 @@ class FeatureViz:
     def visualize(
         self,
         image: Optional[T] = None,
-        num_iterations: int = 100,
-        lr: float = 0.1,
+        thresholds: Optional[Iterable[int]] = (128,),
+        lr: float = 0.05,
         freq: int = 20,
         use_decorrelated: bool = False,
         image_shape: Union[Tuple[int, int], Tuple[int, int, int], int] = (224, 224),
-        plot_images: bool = True,
+        plot_images: bool = False,
+        show_last_image: bool = False,
+        show_progress: bool = True,
+        log_loss: bool = False,
     ) -> List[T]:
         """Main method to visualize the features of the neural network.
 
@@ -72,8 +76,8 @@ class FeatureViz:
         ----------
         image : Optional[T], optional
             The initial image to optimize, by default None. If None, a random image will be created using `torch.randn` and the `image_shape` parameter.
-        num_iterations : int, optional
-            The number of optimization steps, by default 100.
+        thresholds : Optional[Iterable[int]], optional
+            The iterations on which to return the image, by default [128].
         lr : float, optional
             The learning rate of the optimizer, by default 0.1.
         freq : int, optional
@@ -82,6 +86,14 @@ class FeatureViz:
             Whether to use the decorrelated color space, by default False.
         image_shape : Union[Tuple[int, int], Tuple[int, int, int], int], optional
             The shape of the image to optimize, by default (224, 224). If an integer is passed, the image will have the same width and height.
+        plot_images : bool, optional
+            Whether to plot the images while optimizing, by default False. If True, the images will be plotted at an interval of `freq`.
+        show_last_image : bool, optional
+            Whether to show the last image after optimization, by default False. If True, the last image will be displayed.
+        show_progress : bool, optional
+            Whether to show the progress bar, by default True.
+        log_loss : bool, optional
+            Whether to log the loss, by default False. If True, the loss will be logged at each iteration divisible by `freq`.
 
         Returns
         -------
@@ -115,8 +127,8 @@ class FeatureViz:
         image.requires_grad_(True)
 
         optimizer = optim.Adam([image], lr=lr)
-
-        for i in tqdm(range(num_iterations)):
+        num_iterations = max(thresholds) + 1
+        for i in tqdm(range(num_iterations), disable=not show_progress):
             transformed_image = apply_transformations(image).to(self.device)
             optimizer.zero_grad()
             try:
@@ -138,14 +150,25 @@ class FeatureViz:
                 image.clamp_(-1, 1)
 
             if i % freq == 0:
-                self.logger.info(f"Loss: {loss.item()}")
-                image_to_log = image.clone().detach().cpu()
-                images.append(image_to_log)
+                if log_loss:
+                    self.logger.info(f"Loss: {loss.item()}")
+                else:
+                    self.logger.debug(f"Loss: {loss.item()}")
 
+                image_to_log = image.clone().detach().cpu()
                 if is_jupyter_notebook() and plot_images:
                     image_plotter.update_image(
                         image_to_log,
                         title=f"It: {i} | Loss: {loss.item():.2f}",
                     )
+            if i in thresholds:
+                images.append(image.clone().detach().cpu())
         image.detach_()
+        if show_last_image:
+            if is_jupyter_notebook():
+                show_tensor_image(images[-1])
+            else:
+                logger.warning(
+                    "The last image can only be displayed in a Jupyter Notebook."
+                )
         return images
