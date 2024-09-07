334aae6fe91102e434b735a740405bfc95d3f8a3
Harikesh Kushwaha <harikeshkumar0926@gmail.com>
2024-09-07 13:13:16 +0530

    update random image generation methods and the transformation methods
    - Random image generation methods is more generelized to align it with Lucid and Lucent. The image generation function now must return two values: the paramters to optimize and a function that will create the image
    - The transformation methods now returns a function instead of transformed image. This can be used to chain more transformations using a list

--- feat_viz/image.py
+++ feat_viz/image.py
@@ -4,8 +4,10 @@ import torch
 import torchvision.transforms as transforms
 from torchvision.transforms import functional as F
 import numpy as np
-from typing import Literal, Tuple, List, Optional, Union
+from typing import Literal, Tuple, List, Optional, Union, Callable
 
+DEFAULT_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+TORCH_VERSION = torch.__version__
 # Color correlation matrix and normalization
 color_correlation_svd_sqrt = np.array(
     [[0.26, 0.09, 0.02], [0.27, 0.00, -0.05], [0.27, -0.09, 0.03]]
@@ -18,29 +20,35 @@ color_mean = [0.48, 0.46, 0.41]
 def pixel_image(
     shape: Tuple[int, int, int, int],
     sd: Optional[float] = None,
-    device: Optional[str] = None,
-) -> T:
+    device: Optional[str] = DEFAULT_DEVICE,
+) -> Tuple[T, Callable[[], T]]:
     """A naive, pixel-based image parameterization.
     Defaults to a random initialization, but can take a supplied init_val argument
     instead.
 
-    Args:
-      shape: shape of resulting image, [batch, width, height, channels].
-      sd: standard deviation of param initialization noise.
-      init_val: an initial value to use instead of a random initialization. Needs
-        to have the same shape as the supplied shape argument.
+    Parameters
+    ----------
+    shape : Tuple[int, int, int, int]
+        shape of resulting image, [batch, channels, height, width].
+    sd : Optional[float], optional
+        standard deviation of param initialization noise, by default None
+    device : Optional[str], optional
+        device to use for tensor, by default DEFAULT_DEVICE
 
-    Returns:
-      tensor with shape from first argument.
+    Returns
+    -------
+    Tuple[T, Callable[[], T]]
+        Tuple containing the image tensor and a function to generate the image
     """
 
     sd = sd or 0.5
     init_val = np.random.normal(size=shape, scale=sd).astype(np.float32)
     # convert to pytorch tensor
     image = torch.tensor(init_val, device=device)
+    image.requires_grad_(True)
     # convert to batch*channels*height*width
-    image = image.permute(0, 3, 1, 2)
-    return image
+    # image = image.permute(0, 3, 1, 2)
+    return [image], lambda: image
 
 
 def rfft2d_freqs(h, w):
@@ -55,31 +63,42 @@ def rfft2d_freqs(h, w):
     return np.sqrt(fx * fx + fy * fy)
 
 
-def fft_image(shape, sd=None, decay_power=1):
-    """An image parameterization using 2D Fourier coefficients."""
-    sd = sd or 0.5
-    batch, h, w, ch = shape
+def fft_image(shape, sd=None, decay_power=1, device=DEFAULT_DEVICE):
+    sd = sd or 0.05
+    batch, channels, h, w = shape
     freqs = rfft2d_freqs(h, w)
-    init_val_size = (2, batch, ch) + freqs.shape
-    init_val = np.random.normal(size=init_val_size, scale=sd).astype(np.float32)
-    spectrum_real_imag_t = torch.tensor(init_val)
-    spectrum_t = torch.complex(spectrum_real_imag_t[0], spectrum_real_imag_t[1])
+    init_val_size = (
+        (batch, channels) + freqs.shape + (2,)
+    )  # 2 for imaginary and real components
+    sd = sd or 0.01
 
-    # Scale the spectrum. First normalize energy, then scale by the square-root
-    # of the number of pixels to get a unitary transformation.
-    # This allows to use similar learning rates to pixel-wise optimization.
+    spectrum_real_imag_t = (
+        (torch.randn(*init_val_size) * sd).to(device).requires_grad_(True)
+    )
+
     scale = 1.0 / np.maximum(freqs, 1.0 / max(w, h)) ** decay_power
-    scale *= np.sqrt(w * h)
-    scale_t = torch.tensor(scale, dtype=torch.float32)
-    scaled_spectrum_t = scale_t * spectrum_t
+    scale = torch.tensor(scale).float()[None, None, ..., None].to(device)
 
-    # convert complex scaled spectrum to shape (batch, h, w, ch) image tensor
-    image_t = torch.fft.irfft2(scaled_spectrum_t, s=(h, w))
+    def inverse():
+        scaled_spectrum_t = scale * spectrum_real_imag_t
+        if TORCH_VERSION >= "1.7.0":
+            import torch.fft
 
-    # in case of odd spatial input dimensions we need to crop
-    image_t = image_t[:batch, :h, :w, :]
-    image_t = image_t / 4.0
-    return image_t
+            if type(scaled_spectrum_t) is not torch.complex64:
+                scaled_spectrum_t = torch.view_as_complex(scaled_spectrum_t)
+            image = torch.fft.irfftn(scaled_spectrum_t, s=(h, w), norm="ortho")
+        else:
+            import torch
+
+            image = torch.irfft(
+                scaled_spectrum_t, 2, normalized=True, signal_sizes=(h, w)
+            )
+        image = image[:batch, :channels, :h, :w]
+        magic = 4.0  # Magic constant from Lucid library; increasing this seems to reduce saturation
+        image = image / magic
+        return image
+
+    return [spectrum_real_imag_t], inverse
 
 
 def linear_decorelate_color(t: T) -> T:
@@ -107,9 +126,6 @@ def convert_to_valid_rgb(
     """Transform inner dimension of t to valid rgb colors."""
     if decorrelate:
         t = linear_decorelate_color(t)
-    if decorrelate and not sigmoid:
-        color_mean = np.array([0.48, 0.46, 0.41]).reshape(1, 3, 1, 1)
-        t += color_mean
 
     if sigmoid:
         return torch.sigmoid(t)
@@ -133,73 +149,114 @@ def get_image(
     decorrelate: bool = True,
     fft: bool = True,
     alpha=False,
-    sigmoid: bool = True,
+    sigmoid: bool = False,
     scaling_method: Literal["min_max", "norm", "clamp"] = "min_max",
+    device: Optional[str] = DEFAULT_DEVICE,
 ) -> T:
     h = h or w
     batch = batch or 1
-    ch = 4 if alpha else 3
-    shape = [batch, h, w, ch]
+    c = 4 if alpha else 3
+    shape = [batch, c, h, w]
     param_f = fft_image if fft else pixel_image
-    t = param_f(shape, sd=sd)
-    output = convert_to_valid_rgb(
-        t[:, :3, ...],
-        decorrelate=decorrelate,
-        sigmoid=sigmoid,
-        scaling_method=scaling_method,
-    )
-    if alpha:
-        a = torch.nn.functional.sigmoid(
-            t[
-                :,
-                3:,
-                ...,
-            ]
+    params, image_f = param_f(shape, sd=sd, device=device)
+
+    def image():
+        t = image_f()
+        output = convert_to_valid_rgb(
+            t[:, :3, ...],
+            decorrelate=decorrelate,
+            sigmoid=sigmoid,
+            scaling_method=scaling_method,
         )
-        output = torch.cat([output, a], -1)
-    return output
+        if alpha:
+            a = torch.nn.functional.sigmoid(
+                t[
+                    :,
+                    3:,
+                    ...,
+                ]
+            )
+            output = torch.cat([output, a], 1)
+        return output
+
+    return params, image
 
 
-def preprocess_image(image):
-    transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Resize((224, 224)),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ]
-    )
-    return transform(image).unsqueeze(0)
+def random_jitter(max_pixels=16):
+    def inner(image):
+        dx, dy = np.random.randint(-max_pixels, max_pixels + 1, 2)
+        return F.affine(image, 0, (dx, dy), 1, 0)
+
+    return inner
 
 
-def random_jitter(image, max_pixels=16):
-    dx, dy = np.random.randint(-max_pixels, max_pixels + 1, 2)
-    return F.affine(image, 0, (dx, dy), 1, 0)
+def random_scale(min_scale=0.8, max_scale=1.2):
+    def inner(image):
+        scale = np.random.uniform(min_scale, max_scale)
+        return F.affine(image, 0, (0, 0), scale, 0)
+
+    return inner
 
 
-def random_scale(image, min_scale=0.8, max_scale=1.2):
-    scale = np.random.uniform(min_scale, max_scale)
-    return F.affine(image, 0, (0, 0), scale, 0)
+def random_rotate(max_angle=15):
+    def inner(image):
+        angle = np.random.uniform(-max_angle, max_angle)
+        return F.rotate(image, angle)
+
+    return inner
 
 
-def random_rotate(image, max_angle=15):
-    angle = np.random.uniform(-max_angle, max_angle)
-    return F.rotate(image, angle)
+def pad_image(padding=16):
+    def inner(image):
+        return F.pad(image, padding)
+
+    return inner
 
 
-def pad_image(image, padding=16):
-    return F.pad(image, padding)
+def crop_image(x, y):
+    def inner(image):
+        w = image.shape[-1] - x
+        h = image.shape[-2] - y
+        return F.crop(image, x, y, h, w)
+
+    return inner
 
 
-def crop_image(image, x, y, w, h):
-    return F.crop(image, x, y, h, w)
+def normalize():
+    # ImageNet normalization for torchvision models
+    # see https://pytorch.org/docs/stable/torchvision/models.html
+    normal = Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+    def inner(image_t):
+        return torch.stack([normal(t) for t in image_t])
+
+    return inner
 
 
-def apply_transformations(image: T) -> T:
-    og_w, og_h = image.shape[-2:]
-    image = pad_image(image, padding=16)
-    image = random_jitter(image)
-    image = random_scale(image, min_scale=0.95, max_scale=1.05)
-    image = random_rotate(image, max_angle=5)
-    image = random_jitter(image)
-    image = crop_image(image, 16, 16, og_w, og_h)
+def preprocess_inceptionv1():
+    # Original Tensorflow's InceptionV1 model
+    # takes in [-117, 138]
+    # See https://github.com/tensorflow/lucid/blob/master/lucid/modelzoo/other_models/InceptionV1.py#L56
+    # Thanks to ProGamerGov for this!
+    return lambda x: x * 255 - 117
+
+
+DEFAULT_TRANSFORMATIONS = [
+    pad_image(16),
+    random_jitter(16),
+    random_scale(0.95, 1.05),
+    random_rotate(5),
+    random_jitter(16),
+    crop_image(16, 16),
+]
+
+
+def apply_transformations(
+    image: T, extra_transformations: Optional[List[A]] = None
+) -> T:
+    transformations = DEFAULT_TRANSFORMATIONS
+    if extra_transformations:
+        transformations = transformations + extra_transformations
+    for transform in transformations:
+        image = transform(image)
     return image
